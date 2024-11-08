917a07f8570caa7bc4703934122ec366e5e73da1
Saeed Ghanavat <dev_projects@ghanavats.co.uk>
2024-11-08 21:59:48 +0000

    - Tidy up IAggregateRoot.cs with a tiny documentation fix - Fixed a bug with the Domain Factory. It was throwing exception incorrectly that Property Name is invalid

--- src/Infrastructure/CleanArchitecture.Infrastructure/Factories/CreateEntityObjectFactory.cs
+++ src/Infrastructure/CleanArchitecture.Infrastructure/Factories/CreateEntityObjectFactory.cs
@@ -69,13 +69,11 @@ internal class CreateEntityObjectFactory<TRequest, TResponse>
         var objValues = new object?[properties.Count];
         for (var i = 0; i < properties.Count; i++)
         {
-            if (!properties[i].IsValueTypeNullable() || !properties[i].IsReferenceTypeNullable())
-            {
-                var propertyName = properties[i].Name;
-                objValues[i].CheckForNull(() => new NullReferenceException($"Value of property {propertyName} cannot be null or empty."));
-            }
-            
             objValues[i] = properties[i].GetValue(request);
+
+            if (properties[i].IsValueTypeNullable() && properties[i].IsReferenceTypeNullable()) continue;
+            var propertyName = properties[i].Name;
+            objValues[i].CheckForNull(() => new NullReferenceException($"Value of property {propertyName} cannot be null or empty."));
         }
 
         return objValues;
